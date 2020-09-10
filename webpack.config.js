dcab73e3a2e2d1c1df6ae21c59163867ceaae01d
Nick Williams <git@nickawilliams.com>
2020-09-10 15:49:24 -0700

    Build: Added mode to webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const path = require('path')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
+  mode: 'production',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
