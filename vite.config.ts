cd7056212c9d0aa5b7a44e93e8b07552848904e2
Nick Williams <git@nickawilliams.com>
2024-06-04 20:44:08 -0700

    chore: added vite config

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite'
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    envPrefix: 'RESUME_',
+    plugins: [react()],
+    build: {
+      outDir: 'out/build',
+    },
+    server: {
+      port: Number(env.RESUME_PORT ?? 3001),
+    },
+  }
+})
