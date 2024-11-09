a8b395d11a7f5186cdd48428769aa04287070b07
Nick Williams <git@nickawilliams.com>
2024-11-09 13:55:00 -0800

    Chore: upgraded terraform netlify provider, refactored resources to accomodate

--- terraform.tf
+++ terraform.tf
@@ -3,8 +3,8 @@ variable "NETLIFY_TOKEN" {}
 terraform {
   required_providers {
     netlify = {
-      source  = "terraform-providers/netlify"
-      version = "~> 0.4.0"
+      source  = "netlify/netlify"
+      version = "~> 0.1.1"
     }
   }
   required_version = ">= 0.13"
@@ -12,17 +12,26 @@ terraform {
 
 provider "netlify" {
   token = var.NETLIFY_TOKEN
+  default_team_slug = "nickawilliams"
 }
 
-resource "netlify_site" "production" {
-  name          = "nickawilliams"
-  custom_domain = "www.nickawilliams.com"
+data "netlify_site" "production" {
+  id = "aca7217b-8f6a-4dbf-8bbb-2e7a0e49aa5f"
+}
 
-  repo {
-    provider      = "github"
-    repo_path     = "RogWilco/Resume"
-    repo_branch   = "master"
-    command       = "yarn dist"
-    dir           = "dist"
-  }
+resource "netlify_site_domain_settings" "production" {
+  site_id = data.netlify_site.production.id
+  custom_domain = "www.nickawilliams.com"
+}
+
+resource "netlify_site_build_settings" "production" {
+  site_id = data.netlify_site.production.id
+  # functions_region = "us-east-1"
+  build_command = "yarn dist"
+  production_branch = "main"
+  publish_directory = "dist"
+
+  branch_deploy_branches = [
+    "main"
+  ]
 }
